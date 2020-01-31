1d07bb7c19f93a3dbf4ed9c1fda9042d7e0bb571
vladislav nikishov <vladislavnikishov@gmail.com>
2020-02-01 01:27:48 +0300

    конфигурация и простой хендлер

---
+++ cmd/apiserver/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"simple_rest/internal/app/apiserver"
+
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to configuration file")
+}
+
+func main() {
+	flag.Parse()
+
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := apiserver.NewAPIServer(config)
+
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ internal/app/apiserver/apiserver.go
@@ -0,0 +1,61 @@
+package apiserver
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+// APIServer ...
+type APIServer struct {
+	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+}
+
+// NewAPIServer ...
+func NewAPIServer(config *Config) *APIServer {
+	return &APIServer{
+		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
+	}
+}
+
+// Start ...
+func (s *APIServer) Start() error {
+	err := s.configureLogger()
+	if err != nil {
+		return err
+	}
+
+	s.configureRouter()
+	s.logger.Info("starting api server at ", s.config.BindAddr)
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+// configureLogger ...
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
+	return nil
+}
+
+// configureRouter ...
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello")
+	}
+}
